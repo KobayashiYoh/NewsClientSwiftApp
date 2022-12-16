6b3cadeed846f9e625b0a226e83d82605c69f2d5
KobayashiYoh <y.kobayashi@4nonome.com>
2022-12-16 18:38:18 +0900

    first commit

---
+++ NewsClientSwiftApp/NewsClientSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsClientSwiftAppApp.swift
+//  NewsClientSwiftApp
+//
+//  Created by 小林陽 on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct NewsClientSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
