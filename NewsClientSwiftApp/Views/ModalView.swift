843af6deb718e291680e49f4778a6c471665d8cb
KobayashiYoh <y.kobayashi@4nonome.com>
2022-12-26 00:23:20 +0900

    記事をブラウザで表示する機能を追加

--- NewsClientSwiftApp/Views/ModalView.swift
+++ NewsClientSwiftApp/Views/ModalView.swift
@@ -13,13 +13,15 @@ struct ModalView: View {
     var body: some View {
         VStack {
             HStack{
-                Spacer().frame(width: 64).padding()
+                Image(systemName: "globe").onTapGesture(count: 1, perform: {
+                    UIApplication.shared.open(URL(string: url)!)
+                }).foregroundColor(Color.blue).frame(width: 48, alignment: .leading).padding()
                 Spacer()
                 Text("プレビュー").padding().font(.headline)
                 Spacer()
                 Button(action: {
                     self.presentation.wrappedValue.dismiss()
-                }, label: {Text("閉じる")}).frame(width: 64).padding()
+                }, label: {Text("閉じる")}).frame(width: 48).padding()
             }
             WebView(url: url)
         }
